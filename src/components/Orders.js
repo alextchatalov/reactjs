b97c34986c1c323416e6e33f88e586ccebd4afca
Alex Travagin Chatalov <alex.chatalov@matera.com>
2020-06-02 19:13:28 -0300

    removido o login com keycloack

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -7,12 +7,11 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
-
+import api from '../services/api'
 // Generate Order Data
 function createData(id, date, name, shipTo, paymentMethod, amount) {
   return { id, date, name, shipTo, paymentMethod, amount };
 }
-
 const rows = [
   createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
   createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
@@ -33,27 +32,30 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Orders() {
   const classes = useStyles();
+  const [list, setList] = useState([]);
+  useEffect(()=>{
+    api.get("/equipament/list")
+    .then((equipaments)=> setList(equipaments.data));
+  },[]) 
   return (
     <React.Fragment>
       <Title>Recent Orders</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
+            <TableCell>ID</TableCell>
             <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Temperature</TableCell>
+            <TableCell>Location</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+          {list.map((list) => (
+            <TableRow key={list.id}>
+              <TableCell>{list.id}</TableCell>
+              <TableCell>{list.name}</TableCell>
+              <TableCell>{list.temperature}</TableCell>
+              <TableCell>{list.location}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'typeface-roboto';
 import Keycloak from 'keycloak-js'
-
-let initOptions = {
-  url: 'http://127.0.0.1:8080/auth', realm: 'demo', clientId: 'app-react', onLoad: 'login-required'
-}
-
-
-let keycloak = Keycloak(initOptions);
-
-keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
-
-  if (!auth) {
-      window.location.reload();
-  } else {
-      console.info("Authenticated user");
-  }
-
+//
+//let initOptions = {
+//  url: 'http://127.0.0.1:8080/auth', realm: 'demo', clientId: 'app-react', onLoad: 'login-required'
+//}
+//
+//
+//let keycloak = Keycloak(initOptions);
+//
+//keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
+//
+//  if (!auth) {
+//      window.location.reload();
+//  } else {
+//      console.info("Authenticated user");
+//  }
+//
   ReactDOM.render(
     <React.StrictMode>
       <App />
@@ -26,24 +26,25 @@ keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
     document.getElementById('root')
   );
 
-  localStorage.setItem("react-token", keycloak.token);
-  localStorage.setItem("react-refresh-token", keycloak.refreshToken);
-
-  setTimeout(() => {
-      keycloak.updateToken(70).success((refreshed) => {
-          if (refreshed) {
-              console.debug('Token refreshed' + refreshed);
-          } else {
-              console.warn('Token not refreshed, valid for '
-                  + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-          }
-      }).error(() => {
-          console.error('Failed to refresh token');
-      });
-
-
-  }, 60000)
-
-}).error(() => {
-  console.error("Authenticated Failed");
-});
+//  localStorage.setItem("react-token", keycloak.token);
+//  localStorage.setItem("react-refresh-token", keycloak.refreshToken);
+//
+//  setTimeout(() => {
+//      keycloak.updateToken(70).success((refreshed) => {
+//          if (refreshed) {
+//              console.debug('Token refreshed' + refreshed);
+//          } else {
+//              console.warn('Token not refreshed, valid for '
+//                  + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+//          }
+//      }).error(() => {
+//          console.error('Failed to refresh token');
+//      });
+//
+//
+//  }, 60000)
+//
+//}).error(() => {
+//  console.error("Authenticated Failed");
+//});
+//
