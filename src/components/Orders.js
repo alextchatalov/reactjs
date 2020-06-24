f60ea97ce5d435c173991ef1a62179cbcb383927
Alex Travagin Chatalov <alex.chatalov@matera.com>
2020-06-24 15:42:32 -0300

    criacao da lista de valores de investimentos

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -8,17 +8,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 import api from '../services/api'
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
-];
+import Button from '@material-ui/core/Button';
+import CurrencyTextField from '@unicef/material-ui-currency-textfield'
+import Moment from 'react-moment';
 
 function preventDefault(event) {
   event.preventDefault();
@@ -33,29 +25,88 @@ const useStyles = makeStyles((theme) => ({
 export default function Orders() {
   const classes = useStyles();
   const [list, setList] = useState([]);
+  const totalAppliedAmount = 0;
+  
   useEffect(()=>{
-    api.get("/equipament/list")
-    .then((equipaments)=> setList(equipaments.data));
+    api.get("/investiment/list")
+    .then((investiment)=> setList(investiment.data));
+    for (var i=0; i<list.length; i++) {
+      totalAppliedAmount += list[i].appliedAmount;
+      console.log(list[i].appliedAmount)
+    }
   },[]) 
+
+  function onChangeHandler(e) {
+    console.log("UPLOAD")
+    console.log(e.target.files[0])
+    const fd = new FormData();
+    fd.append('files', e.target.files[0])
+    api.post("/investiment/upload",fd).then(res => {console.log(res)})
+  }
+
   return (
     <React.Fragment>
+      Total: {totalAppliedAmount}
+      <Button
+        variant="contained"
+        component="label">
+        Upload
+        <input
+          type="file"
+          style={{ display: "none" }}
+          onChange={onChangeHandler}/>
+      </Button>
       <Title>Recent Orders</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Temperature</TableCell>
-            <TableCell>Location</TableCell>
+            <TableCell>Ação</TableCell>
+            <TableCell>Tipo</TableCell>
+            <TableCell>Corretora</TableCell>
+            <TableCell>Data de Aplicação</TableCell>
+            <TableCell>Valor Aplicado</TableCell>
+            <TableCell>Saldo</TableCell>
+            <TableCell>Rentabilidade</TableCell>
+            <TableCell>Porcentagem da Carteira</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {list.map((list) => (
-            <TableRow key={list.id}>
-              <TableCell>{list.id}</TableCell>
-              <TableCell>{list.name}</TableCell>
-              <TableCell>{list.temperature}</TableCell>
-              <TableCell>{list.location}</TableCell>
+            <TableRow key={list.investimentCode}>
+              <TableCell>{list.investimentCode}</TableCell>
+              <TableCell>{list.type}</TableCell>
+              <TableCell>{list.broker}</TableCell>
+              <TableCell> <Moment format="DD/MM/YYYY">{list.firstDateApplication}</Moment></TableCell>
+              <TableCell> 
+                <CurrencyTextField
+                  value={list.appliedAmount}
+                  currencySymbol="R$"
+                  decimalCharacter=","
+                  digitGroupSeparator="."
+                  disabled
+                />
+              </TableCell>
+              <TableCell>
+              <CurrencyTextField
+                  value={list.balance}
+                  currencySymbol="R$"
+                  decimalCharacter=","
+                  digitGroupSeparator="."
+                  disabled
+                />
+              </TableCell>
+              <TableCell>
+              <CurrencyTextField
+                  value={list.rentail}
+                  currencySymbol="R$"
+                  decimalCharacter=","
+                  digitGroupSeparator="."
+                  disabled
+                />
+              </TableCell>
+              <TableCell>
+              {list.portfolioShare} %
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
